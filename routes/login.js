fac19e1b6e0a206f7ab39317ee41a42d4acde6e6
swanhell <swanhell@sfu.ca>
2018-03-28 20:04:38 -0700

    login user

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
 router.get('/', function (req, res, next) {
-    res.render('login', { title: 'Login' });
+    res.render('login', {
+        title: 'Login'
+    });
 });
 
 router.post('/', (req, res, next) => {
-    res.redirect('../')
+    User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    }, (err, user) => {
+        if (err) {
+            res.render('login', {
+                title: 'Login',
+                error: `Error: ${err}`
+            });
+        } else if (!user) {
+            res.render('login', {
+                title: 'Login',
+                error: 'Username or password was incorrect'
+            });
+        } else {
+            res.redirect('../');
+        }
+    });
 });
 
 module.exports = router;
