00112fbf988e3460e5d703a33656cd4294d3900c
swanhell <swanhell@sfu.ca>
2018-04-06 15:30:01 -0700

    refactor cubes

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,5 +1,6 @@
 var camera, controls, scene, renderer;
 var cube_1;
+var cubes = [];
 init();
 animate();
 function init() {
@@ -12,159 +13,103 @@ function init() {
     controls = new THREE.TrackballControls(camera);
     controls.addEventListener('change', render);
     scene = new THREE.Scene();
+
+    for (var i= 0; i < 27; i++) {
+        cubes.push(new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial()));
+    }
+
     //1st Layer
     //1 row
-    cube_1 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshBasicMaterial());
-    var cube_2 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_3 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-
-    cube_2.position.x = 300;
-    cube_3.position.x = 600;
+    cubes[1].position.x = 300;
+    cubes[2].position.x = 600;
+    
     //2 row
-    var cube_4 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_5 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_6 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
+    cubes[3].position.y = 300;
+    cubes[4].position.y = 300;
+    cubes[5].position.y = 300;
 
-    cube_4.position.y = 300;
-    cube_5.position.y = 300;
-    cube_6.position.y = 300;
-
-    cube_5.position.x = 300;
-    cube_6.position.x = 600;
+    cubes[4].position.x = 300;
+    cubes[5].position.x = 600;
+    
     //3 row
-    var cube_7 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_8 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_9 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
+    cubes[6].position.y = 600;
+    cubes[7].position.y = 600;
+    cubes[8].position.y = 600;
 
-    cube_7.position.y = 600;
-    cube_8.position.y = 600;
-    cube_9.position.y = 600;
-
-    cube_8.position.x = 300;
-    cube_9.position.x = 600;
+    cubes[7].position.x = 300;
+    cubes[8].position.x = 600;
 
     //2nd Layer
     //1
-    var cube_10 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_11 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_12 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
+    cubes[9].position.z = 300;
+    cubes[10].position.z = 300;
+    cubes[11].position.z = 300;
 
-    cube_10.position.z = 300;
-    cube_11.position.z = 300;
-    cube_12.position.z = 300;
-
-    cube_11.position.x = 300;
-    cube_12.position.x = 600;
+    cubes[10].position.x = 300;
+    cubes[11].position.x = 600;
 
     //2
-    var cube_13 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_14 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_15 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
+    cubes[12].position.z = 300;
+    cubes[13].position.z = 300;
+    cubes[14].position.z = 300;
 
-    cube_13.position.z = 300;
-    cube_14.position.z = 300;
-    cube_15.position.z = 300;
+    cubes[12].position.y = 300;
+    cubes[13].position.y = 300;
+    cubes[14].position.y = 300;
 
-    cube_13.position.y = 300;
-    cube_14.position.y = 300;
-    cube_15.position.y = 300;
-
-    cube_14.position.x = 300;
-    cube_15.position.x = 600;
+    cubes[13].position.x = 300;
+    cubes[14].position.x = 600;
 
     //3
-    var cube_16 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_17 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_18 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
+    cubes[15].position.z = 300;
+    cubes[16].position.z = 300;
+    cubes[17].position.z = 300;
 
-    cube_16.position.z = 300;
-    cube_17.position.z = 300;
-    cube_18.position.z = 300;
+    cubes[15].position.y = 600;
+    cubes[16].position.y = 600;
+    cubes[17].position.y = 600;
 
-    cube_16.position.y = 600;
-    cube_17.position.y = 600;
-    cube_18.position.y = 600;
-
-    cube_17.position.x = 300;
-    cube_18.position.x = 600;
+    cubes[16].position.x = 300;
+    cubes[17].position.x = 600;
 
     //3rd Layer
     //1
-    var cube_19 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_20 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_21 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
+    cubes[18].position.z = 600;
+    cubes[19].position.z = 600;
+    cubes[20].position.z = 600;
 
-    cube_19.position.z = 600;
-    cube_20.position.z = 600;
-    cube_21.position.z = 600;
-
-    cube_20.position.x = 300;
-    cube_21.position.x = 600;
+    cubes[19].position.x = 300;
+    cubes[20].position.x = 600;
 
     //2
-    var cube_22 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_23 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_24 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
+    cubes[21].position.z = 600;
+    cubes[22].position.z = 600;
+    cubes[23].position.z = 600;
 
-    cube_22.position.z = 600;
-    cube_23.position.z = 600;
-    cube_24.position.z = 600;
+    cubes[21].position.y = 300;
+    cubes[22].position.y = 300;
+    cubes[23].position.y = 300;
 
-    cube_22.position.y = 300;
-    cube_23.position.y = 300;
-    cube_24.position.y = 300;
-
-    cube_23.position.x = 300;
-    cube_24.position.x = 600;
+    cubes[22].position.x = 300;
+    cubes[23].position.x = 600;
 
     //3
-    var cube_25 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_26 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
-    var cube_27 = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshNormalMaterial());
+    cubes[24].position.z = 600;
+    cubes[25].position.z = 600;
+    cubes[26].position.z = 600;
 
-    cube_25.position.z = 600;
-    cube_26.position.z = 600;
-    cube_27.position.z = 600;
+    cubes[24].position.y = 600;
+    cubes[25].position.y = 600;
+    cubes[26].position.y = 600;
 
-    cube_25.position.y = 600;
-    cube_26.position.y = 600;
-    cube_27.position.y = 600;
-
-    cube_26.position.x = 300;
-    cube_27.position.x = 600;
+    cubes[25].position.x = 300;
+    cubes[26].position.x = 600;
 
 
 
-
-    scene.add(cube_1);
-    scene.add(cube_2);
-    scene.add(cube_3);
-    scene.add(cube_4);
-    scene.add(cube_5);
-    scene.add(cube_6);
-    scene.add(cube_7);
-    scene.add(cube_8);
-    scene.add(cube_9);
-    scene.add(cube_10);
-    scene.add(cube_11);
-    scene.add(cube_12);
-    scene.add(cube_13);
-    scene.add(cube_14);
-    scene.add(cube_15);
-    scene.add(cube_16);
-    scene.add(cube_17);
-    scene.add(cube_18);
-    scene.add(cube_19);
-    scene.add(cube_20);
-    scene.add(cube_21);
-    scene.add(cube_22);
-    scene.add(cube_23);
-    scene.add(cube_24);
-    scene.add(cube_25);
-    scene.add(cube_26);
-    scene.add(cube_27);
-
-
+    for (var cube of cubes) {
+        scene.add(cube);
+    }
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight)
